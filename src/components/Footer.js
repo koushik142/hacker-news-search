eb52d4adefd99e8e6232e9cb5ee69440a7710ffe
Koushik tatikondawar <koushikkumar142@gmail.com>
2022-07-29 10:58:51 +0530

    Added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Designed by <span className="green-text">Koushik</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
